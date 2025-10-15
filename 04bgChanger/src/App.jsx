a679f9d4f168ec417b2570b31896d386a01e90da
Mansi More <moremansi0817@gmail.com>
2025-10-16 02:38:05 +0530

    Initialize 04bgChanger project with React, Vite, and Tailwind CSS setup
    - Created package.json with dependencies and scripts for development
    - Added PostCSS configuration for Tailwind CSS and Autoprefixer
    - Included Vite logo SVG in the public directory
    - Created App.css for styling the application
    - Developed App.jsx component with color-changing functionality
    - Added React logo SVG in assets
    - Set up index.css for Tailwind CSS directives
    - Implemented main.jsx for rendering the App component
    - Configured Tailwind CSS with content paths
    - Established Vite configuration for the project

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      ></div>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "red" }} onClick={() => setColor("red")}>Red</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "pink" }} onClick={() => setColor("pink")}>Pink</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "green" }} onClick={() => setColor("green")}>Green</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "yellow" }} onClick={() => setColor("yellow")}>Yellow</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "orange" }} onClick={() => setColor("orange")}>Orange</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "magenta" }} onClick={() => setColor("magenta")}>Magenta</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "purple" }} onClick={() => setColor("purple")}>Purple</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "brown" }} onClick={() => setColor("brown")}>Brown</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: "gray" }} onClick={() => setColor("gray")}>Gray</button>
+
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
